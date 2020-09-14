8e91fb383fe48850cf72da6fc9d6fa2158f0f695
Youhei Oonuma <oonuma617@gmail.com>
2020-09-14 22:45:56 +0900

    use realtime database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +8,17 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", {structuredData: true});
     response.send("Hello from Firebase!");
+
+    const database = admin.database();
+    const ref = database.ref("youtube_data/yukihanaramili");
+
+    ref.once("value")
+        .then(value => {
+            functions.logger.info("Hello logs!", value.val());
+        })
+        .catch(reason => {
+            functions.logger.error(reason);
+        })
 });
+
+admin.initializeApp(functions.config().firebase)
